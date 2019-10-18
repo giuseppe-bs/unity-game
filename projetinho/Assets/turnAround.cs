22b390ce1c2e96d539f119fa422e813eed5fcb57
minipepsi-trueversion <54672325+minipepsi-trueversion@users.noreply.github.com>
2019-10-18 11:41:48 -0300

    projeto criado

---
+++ projetinho/Assets/turnAround.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turnAround : MonoBehaviour {
+
+    // Use this for initialization
+    public float rotateSpeed = 10f;
+
+	void OnMouseDrag () {
+        float rotX = Input.GetAxis("Mouse X") * rotateSpeed * Mathf.Deg2Rad;
+        float roty = Input.GetAxis("Mouse Y") * rotateSpeed * Mathf.Deg2Rad;
+
+        transform.RotateAround(Vector3.up, -rotX);
+        transform.RotateAround(Vector3.right, roty);
+    }
+}
