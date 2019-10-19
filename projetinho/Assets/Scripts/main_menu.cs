5354d0f1138449390b11def90fd3881d10b32fb0
minipepsi-trueversion <54672325+minipepsi-trueversion@users.noreply.github.com>
2019-10-19 14:49:39 -0300

    Menu pronto

---
+++ projetinho/Assets/Scripts/main_menu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class main_menu : MonoBehaviour {
+    
+    public void loadGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
