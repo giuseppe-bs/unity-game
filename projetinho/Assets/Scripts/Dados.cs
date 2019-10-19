9e1791025fc99b6ed3da6e804958305a5338a380
minipepsi-trueversion <54672325+minipepsi-trueversion@users.noreply.github.com>
2019-10-19 17:15:00 -0300

    mudone

---
+++ projetinho/Assets/Scripts/Dados.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dados : MonoBehaviour {
+   /* private Text ag = GameObject.Find("Agua");
+    private Text al = GameObject.Find("Alimentos");
+    private Text ene = GameObject.Find("Energia");
+    private Text tec = GameObject.Find("Tecnologia");
+    private Text rec = GameObject.Find("Recursos");
+    */
+    public float agua;
+    public float alimento;
+    public float energia;
+    public float tecnologia;
+    public float recursos;
+    void Start () {
+        agua = 0.10f;
+        alimento = 0.10f;
+        energia = 0.4f;
+        tecnologia = 0.6f;
+        recursos = 0.4f;
+	}
+	
+	// Update is called once per frame
+	public void novoturno () {
+		
+	}
+}
