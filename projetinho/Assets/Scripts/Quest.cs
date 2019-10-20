9d7be86b22e231b9280130fff3698a30b36de89e
minipepsi-trueversion <54672325+minipepsi-trueversion@users.noreply.github.com>
2019-10-20 15:20:20 -0300

    Ultimo

--- projetinho/Assets/Scripts/Quest.cs
+++ projetinho/Assets/Scripts/Quest.cs
@@ -4,30 +4,22 @@ using UnityEngine;
 
 public class Quest : MonoBehaviour {
     private GameObject t;
-    private GameObject quest;
-    private GameObject Question;
-    private GameObject Awnswer;
+    private GameObject quiz;
+    private GameObject question;
+    private GameObject resposta;
     private GameObject Earth;
 
     void Start()
     {
         t = GameObject.Find("Dados");
-        quest = GameObject.Find("Quiz1");
-        Awnswer = GameObject.Find("Answer");
-        Question = GameObject.Find("Pergunta");
-        Earth = GameObject.Find("lowpoly_earth");
     }
 
     public void acerto()
     {
         t.GetComponent<rounds>().sciencepoints += 2;
-        quest.SetActive(false);
-        Earth.SetActive(true);
     }
     public void erro()
     {
         t.GetComponent<rounds>().sciencepoints -= 1;
-        Awnswer.SetActive(true);
-        Question.SetActive(false);
     }
 }
